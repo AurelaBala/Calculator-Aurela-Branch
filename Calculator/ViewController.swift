a24acb55fad9504dd7b468ec57d3bf1d933b9769
AurelaBala <aurelatosuni2022@gmail.com>
2022-09-20 22:16:41 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Adriana Diaz Torres on 9/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var CalculationLabel: UILabel!
+    @IBOutlet weak var ResultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
